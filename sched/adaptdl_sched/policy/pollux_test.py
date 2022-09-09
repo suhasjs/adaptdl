912542dc830049fce05da47376236066a3bb9e9f
Suhas Jayaram Subramanya <j.s.suhas@live.in>
2022-09-08 23:31:21 -0400

    fixed broken test for PolluxPolicy using new data from CIFAR10 on phodgx1

--- sched/adaptdl_sched/policy/pollux_test.py
+++ sched/adaptdl_sched/policy/pollux_test.py
@@ -24,51 +24,64 @@ from adaptdl_sched.policy.speedup import SpeedupFunction
 from adaptdl_sched.policy.utils import JobInfo, NodeInfo
 
 
-@pytest.mark.parametrize("num_nodes", [1, 2, 4, 8, 16])
-def test_optimize(num_nodes, total_devices=16):
-    assert total_devices % num_nodes == 0
-    num_devices = total_devices // num_nodes
-    print("{}x{} nodes:".format(num_nodes, num_devices))
-    # Make up a realistic speedup function.
-    perf_params = PerfParams(0.121, 0.00568, 0.0236, 0.00634,
-                             0.0118, 0.00317, 1.14)
-    grad_params = GradParams(sqr=0.00136, var=0.000502)
-    goodput_fn = GoodputFunction(perf_params, grad_params, 128)
-    speedup_fn = SpeedupFunction(goodput_fn, max_batch_size=1280,
-                                 atomic_bsz_range=(64, 256))
-    now = datetime.now()
-    jobs = {}
-    # Add a few jobs.
-    job_resources = {"nvidia.com/gpu": 1, "pods": 1}
-    for i in range(16):
-        creation_timestamp = now + timedelta(minutes=len(jobs)),
-        max_replicas = 8
-        min_replicas = 0
-        key = len(jobs)
-        jobs[key] = JobInfo(job_resources, speedup_fn, creation_timestamp,
-                            min_replicas, max_replicas)
-    # Add a few nodes.
-    node_resources = {"nvidia.com/gpu": num_devices, "pods": 32}
-    nodes = {i: NodeInfo(node_resources, preemptible=False)
-             for i in range(num_nodes)}
-    # Add a node template.
-    node_template = NodeInfo(node_resources, preemptible=True)
-    policy = PolluxPolicy()
-    prev_allocs = {}
-    for i in range(3):
-        start = time.time()
-        allocations, desired_nodes = \
-            policy.optimize(jobs, nodes, prev_allocs, node_template)
-        duration = time.time() - start
-        print("optimize {}x ({}s sec):".format(i + 1, duration))
-        node_count = Counter()
-        for job_key, placement in allocations.items():
-            assert len(placement) <= jobs[job_key].max_replicas
-            for node_key in placement:
-                node_count[node_key] += 1
-        for node_key, count in node_count.items():
-            assert count <= nodes[node_key].resources["nvidia.com/gpu"]
-            assert count <= nodes[node_key].resources["pods"]
+# using perf params for 4-GPUs cifar10 on phodgx1 (~2m into training)
+def test_optimize(num_nodes, total_devices=8):
+  assert total_devices % num_nodes == 0
+  num_devices = total_devices // num_nodes
+  print("{}x{} nodes:".format(num_nodes, num_devices))
+  # Make up a realistic speedup function.
+  '''
+  perf_params = PerfParams(0.121, 0.00568, 0.0236, 0.00634,
+               0.0118, 0.00317, 1.14)
+  '''
+  perf_params = PerfParams(0.023, 9.08e-5, 0.0133, 0.008036,
+                           0.012109, 0.007306, 1.914794)
+  grad_params = GradParams(sqr=0.10985, var=2.94965)
+  goodput_fn = GoodputFunction(perf_params, grad_params, 128)
+  speedup_fn = SpeedupFunction(goodput_fn, max_batch_size=4096,
+                 atomic_bsz_range = (32, 4096), accumulation=True)
+  now=datetime.now()
+  jobs={}
+  # Add a few jobs.
+  job_resources={'pods': 1, 'cpu': 9, 'memory': 40, 'nvidia.com/gpu': 1, 'rdma/hca': 1}
+  for i in range(2):
+    creation_timestamp=now,
+    max_replicas=8
+    min_replicas=0
+    key="cifar10-"+str(i)
+    jobs[key]=JobInfo(job_resources, speedup_fn, creation_timestamp,
+              min_replicas, max_replicas)
+    jobs[key].attained_service = 547.4
+    jobs[key].num_restarts = 2
+    jobs[key].age = 273
+
+  # Add a few nodes == phodgx1
+  node_resources = {'cpu': 255, 
+           'memory': 1081,
+           'nvidia.com/gpu': num_devices, 
+           'pods': 84,
+           'rdma/hca': num_devices}
+  nodes = {i: NodeInfo(node_resources, preemptible=False)
+       for i in range(num_nodes)}
+  # Add a node template.
+  node_template = NodeInfo(node_resources, preemptible=True)
+  policy = PolluxPolicy()
+  prev_allocs = {}
+  for i in range(3):
+    start = time.time()
+    allocations, desired_nodes = \
+      policy.optimize(jobs, nodes, prev_allocs, node_template)
+    print(f"Allocations: {allocations}")
+    duration = time.time() - start
+    print("optimize {}x ({}s sec):".format(i + 1, duration))
+    node_count = Counter()
+    for job_key, placement in allocations.items():
+      assert len(placement) <= jobs[job_key].max_replicas
+      for node_key in placement:
+        node_count[node_key] += 1
+    for node_key, count in node_count.items():
+      assert count <= nodes[node_key].resources["nvidia.com/gpu"]
+      assert count <= nodes[node_key].resources["pods"]
 
 def test_unusable_node():
     # Test where one of the nodes can't be used due to one resource type.
@@ -102,3 +115,7 @@ def test_unusable_node():
     assert max(len(alloc) for alloc in allocations.values()) == 1
     # Check two jobs were allocated.
     assert sum(len(alloc) for alloc in allocations.values()) == 2
+
+if __name__ == "__main__":
+  for i in [1, 2, 4, 8, 16]:
+    test_optimize(1, i)
